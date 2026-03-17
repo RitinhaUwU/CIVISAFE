a7eccdc6df652c940413b3fccf5670cb1b57427a
João Gonçalves <2231775@my.ipleiria.pt>
2026-03-17 23:39:44 +0000

    Troca de imagem da DB para postgis. Inicio de enpoints de autenticação

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages([
+                'email' => ['As credenciais fornecidas estao incorretas.'],
+            ]);
+        }
+
+        $user = Auth::user();
+        if ($user->blocked === true)
+        {
+            abort(401,'Esta conta esta bloqueada');
+        }
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(Request $request): Response
+    {
+        $request->user()->tokens()->delete();
+        return response()->noContent();
+    }
+}
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,6 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui',
     '@vueuse/nuxt',
-    '@pinia/nuxt',
     '@vite-pwa/nuxt'
   ],
 
