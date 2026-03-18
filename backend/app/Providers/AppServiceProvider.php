448c4cf627a53a9f3236bef5d54748cb1be63173
João Gonçalves <2231775@my.ipleiria.pt>
2026-03-18 19:09:36 +0000

    Upgrade Laravel 13. Instalado ActivityLog

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Support\ServiceProvider;
-use Symfony\Component\HttpFoundation\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
