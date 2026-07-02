0e9d26d8a297482949c192f8a308b802addc84b5
Inês Arnauth <2231776@my.ipleiria.pt>
2026-07-02 03:58:09 +0100

    Fix: Problemas a atualizar a password de outros utilizadores

--- backend/app/Http/Requests/UserUpdateRequest.php
+++ backend/app/Http/Requests/UserUpdateRequest.php
@@ -11,15 +11,20 @@ class UserUpdateRequest extends FormRequest
     {
         $is_patch = $this->isMethod('PATCH');
 
-        return [
+        $rules = [
             'name' => [$is_patch ? 'sometimes' : 'required', 'min:1'],
             'email' => [$is_patch ? 'sometimes' : 'required', 'email', 'max:255', Rule::unique('users', 'email')->ignore($this->route('user'))],
-            'current_password' => ['required_with:password', 'current_password'],
             'password' => [$is_patch ? 'sometimes' : 'nullable', 'string', 'min:8', 'confirmed'],
             'mobile' => [$is_patch ? 'sometimes' : 'required', 'regex:/^\+?[0-9]+(?: [0-9]+)*$/'],
             'locked' => [$is_patch ? 'sometimes' : 'required', 'boolean'],
             'role' => [$is_patch ? 'sometimes' : 'required', 'string', 'exists:roles,name'],
         ];
+
+        if (auth()->id() == $this->route('user')->id) {
+            $rules['current_password'] = ['required_with:password', 'current_password'];
+        }
+
+        return $rules;
     }
 
     public function authorize(): bool
